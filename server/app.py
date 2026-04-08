281e65b84647acc42424e0bb3adc4611daac8695
Sanket Dhuri <sanketdhuri9604@gmail.com>
2026-04-08 08:47:01 +0530

    Resolve

---
+++ server/app.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app
+
+def main():
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+if __name__ == '__main__':
+    main()
